ec92397168a1cc32303ce234ff7e865bcc1d66ca
Michael Grabarek <michael.grabarek@gmail.com>
2024-09-01 17:44:49 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        String a = "Hello World!";
+        String b = "Michael";
+        int c = 28;
+        char d = 'G';
+                System.out.println(a + " " + b + " " + c + " "+ d);
+    }
+}
