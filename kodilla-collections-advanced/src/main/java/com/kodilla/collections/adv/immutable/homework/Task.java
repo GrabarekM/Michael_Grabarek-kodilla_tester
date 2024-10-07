ea528ae374a727109807c7ecc3402d3ef997095a
Michael Grabarek <michael.grabarek@gmail.com>
2024-10-07 12:41:28 +0200

    5.3 immutables

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    protected String title;
+    protected int duration;
+    public Task(String title, int duration){
+        this.title = title;
+        this.duration = duration;
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApplication {
+    public static void main(String[] args) {
+        TaskRecord taskRecord = new TaskRecord("Testing", 123, 123);
+    }
+}
